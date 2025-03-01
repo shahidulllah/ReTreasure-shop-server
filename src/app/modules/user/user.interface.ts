bdafb58bf0fca8d1dcbff7aed8f80e1fdbf299a2
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-02 02:19:38 +0600

    user model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,6 +1,6 @@
 export interface IUser {
-    name: string;
-    email: string;
-    password: string;
-  }
-  
+  name: string;
+  email: string;
+  password: string;
+  role: "user" | "admin";
+}
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,11 +1,35 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcryptjs";
 import { IUser } from "./user.interface";
 
-const UserSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
+export interface IUserModel extends IUser, Document {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<IUserModel>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-const User = mongoose.model<IUser>("User", UserSchema);
+// Compare passwords
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUserModel>("User", UserSchema);
 export default User;

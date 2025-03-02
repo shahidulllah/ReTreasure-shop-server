1157634a4c73af427b7e915c417ce139934f5b81
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-02 23:41:03 +0600

    payment controller

---
+++ src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import * as paymentService from "./payment.service";
+
+interface AuthenticatedRequest extends Request {
+  user?: {
+    id: string; 
+  };
+}
+
+// Create a new payment
+export const createPayment = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const { orderId, amount } = req.body;
+    const userId = req.user?.id;
+    if (!userId) {
+      res.status(401).json({ message: "Unauthorized. Please log in." });
+      return;
+    }
+
+    const clientSecret = await paymentService.createPaymentIntent(
+      orderId,
+      amount,
+      userId
+    );
+    res.status(201).json({
+      success: true,
+      message: "Payment intent created successfully.",
+      clientSecret,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: "Failed to create payment intent.",
+      error: error.message,
+    });
+  }
+};
+
+// Update payment status
+export const updatePayment = async (req: Request, res: Response) => {
+  try {
+    const { paymentIntentId, status } = req.body;
+
+    if (!paymentIntentId || !status) {
+      res
+        .status(400)
+        .json({ message: "PaymentIntent ID and status are required." });
+      return;
+    }
+
+    const payment = await paymentService.updatePaymentStatus(
+      paymentIntentId,
+      status
+    );
+
+    if (!payment) {
+      res.status(404).json({ message: "Payment not found." });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Payment status updated successfully.",
+      payment,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: "Failed to update payment status.",
+      error: error.message,
+    });
+  }
+};
---
+++ src/app/modules/payment/payment.service.ts
@@ -0,0 +1,36 @@
+import { stripe } from "../../config/stripe";
+import Payment from "./payment.model";
+import { IPayment } from "./payment.interface";
+
+export const createPaymentIntent = async (
+  orderId: string,
+  amount: number,
+  userId: string
+) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount * 100, 
+    currency: "usd",
+  });
+
+  const payment = await Payment.create({
+    user: userId,
+    order: orderId,
+    amount,
+    status: "pending",
+    paymentIntentId: paymentIntent.id,
+  });
+
+  return paymentIntent.client_secret;
+};
+
+//Update payment status
+export const updatePaymentStatus = async (
+  paymentIntentId: string,
+  status: "paid" | "failed"
+) => {
+  return await Payment.findOneAndUpdate(
+    { paymentIntentId },
+    { status },
+    { new: true }
+  );
+};

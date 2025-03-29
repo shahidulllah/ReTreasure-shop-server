d33fe4037bffa28e48a98d895a884b1bc9fc23cc
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-29 15:36:16 +0600

    wishlist route

---
+++ src/app/modules/wishlist/wishlist.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/" );
+router.get("/", );
+router.get("/:id", );
+router.delete("/:id",);
+
+export const wishlistRoutes = router;

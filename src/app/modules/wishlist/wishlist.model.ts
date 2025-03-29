57c33eecf08b33b3040a3104723be1f15fbc4ec0
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-29 14:53:34 +0600

    wishlist model

---
+++ src/app/modules/wishlist/wishlist.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import { IListing } from "../listings/listings.interface";
+
+
+const WishlistSchema = new Schema<IListing>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    condition: { type: String, required: true },
+    location: { type: String, required: true },
+    image: { type: String, default: "https://ibb.co.com/5CJnLF6" },
+    sellerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ListingModel = mongoose.model<IListing>("Wishlist", WishlistSchema);

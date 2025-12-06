382e0cbf8fe0005c66c86a03afef99decc448e15
shahidulllah <mdshahidsumon177@gmail.com>
2025-12-06 21:39:26 +0600

    image uploader controller modify

--- src/app/modules/upload/upload.route.ts
+++ src/app/modules/upload/upload.route.ts
@@ -2,9 +2,8 @@ import { Router } from "express";
 import { upload } from "../../middlewares/multer";
 import { uploadImage } from "./upload.controller";
 
-
 const router = Router();
 
 router.post("/upload", upload.single("file"), uploadImage);
 
-export const uploadRoutes=router;
+export const uploadRoutes = router;

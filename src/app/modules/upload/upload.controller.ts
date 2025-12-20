bc899fb0103a94f8f3154a8d2f78a9853a679863
shahidulllah <mdshahidsumon177@gmail.com>
2025-12-20 10:13:16 +0600

    code refectoring

--- src/app/modules/upload/upload.controller.ts
+++ src/app/modules/upload/upload.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import cloudinary from "../../config/cloudinary";
 
 export const uploadImage = async (req: Request, res: Response) => {
-  console.log("file: ",req.file);
   try {
     if (!req.file) {
       res.status(400).json({ message: "No file uploaded" });
